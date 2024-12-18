90a8755fa70beaa777a896986d72776cba7a25fc
Mohammed-ela <elamrani.mohammed95@gmail.com>
2024-12-18 15:27:00 +0100

    Configuration du store global avec Context API

---
+++ src/store/GlobalStore.js
@@ -0,0 +1,13 @@
+import React, { createContext, useState } from 'react';
+
+export const GlobalContext = createContext();
+
+export const GlobalProvider = ({ children }) => {
+  const [userInfo, setUserInfo] = useState({ name: '', surname: '' });
+
+  return (
+    <GlobalContext.Provider value={{ userInfo, setUserInfo }}>
+      {children}
+    </GlobalContext.Provider>
+  );
+};
