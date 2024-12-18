43c25f5fa921f0aef6ea6095beb886cb99dcdcd9
Mohammed-ela <elamrani.mohammed95@gmail.com>
2024-12-18 21:59:54 +0100

    update librairie jest , podfile package

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,14 @@
 module.exports = {
   preset: 'react-native',
+  setupFilesAfterEnv: ['@testing-library/react-native/jest-preset'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  transformIgnorePatterns: [
+    'node_modules/(?!(react-native' +
+      '|@react-native' +
+      '|react-native-qrcode-svg' +
+      ')/)',
+  ],
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|svg)$': '<rootDir>/__mocks__/fileMock.js',
+  },
 };
